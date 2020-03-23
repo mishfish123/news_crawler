ac2020bb74e1aaf26bb7808aade0e45b65ebb00a
Marcel <marcel.masques@gmail.com>
2020-03-23 20:57:40 +1100

    Added the option to export as a CSV

--- newscrawler.py
+++ newscrawler.py
@@ -3,8 +3,9 @@ from datetime import datetime, timedelta, timezone
 import dateparser
 from json import dumps 
 from dateutil import tz
+import csv
 
-def newscrawler(rss_links_name, hour_offset, filter_by, file_to_write):
+def newscrawler(rss_links_name, hour_offset, filter_by, file_to_write, output_type='csv'):
 	"""
 	Crawls through the RSS links specified to get news, filtering by hour_offset (how old do you want the news) and
 	by keywords (filter_by)
@@ -13,10 +14,10 @@ def newscrawler(rss_links_name, hour_offset, filter_by, file_to_write):
 		rss_links_name: {string} name of a file containing RSS links to crawl.
 		hour_offset: 	{int} number of hours old an article can be
 		filter_by:		{[string]} list of keywords. Article title or summary must have at least one keyword to be included
-		file_to_write:	{string} name of file to write .json output to. NOTE: overriding data in file given. 
-
+		file_to_write:	{string} name of file to write .json/csv output to. NOTE: overriding data in file given. 
+		output_type: {string} "csv" for csv or "json" for json. (anything else will default to csv)
 	Out:
-		.json file with news 
+		.json file with news or .csv file with news 
 	"""
 	##### read link file #####
 	rss_file = open(rss_links_name, 'r')
@@ -29,7 +30,6 @@ def newscrawler(rss_links_name, hour_offset, filter_by, file_to_write):
 
 	##### Get the current news related to the filters ####
 	news_articles = {'updateTime': [{'time': now.strftime('%I:%M%p %d %B %Y')}], 'news': []}; # create json object 
-
 	for url in rss_feeds:
 		d = feedparser.parse(url)
 		try: 
@@ -47,12 +47,27 @@ def newscrawler(rss_links_name, hour_offset, filter_by, file_to_write):
 					print_time = article_date.time().strftime('%I:%M%p')
 					news_articles['news'].append({'url': entry.link, 'title': entry.title, 'source': author, 'date': print_date, 'time': print_time})
 
+	# sort by date
+	news_articles['news'] = sorted(news_articles['news'], key = lambda i: (datetime.strptime(i['date'],'%d %B %Y') , datetime.strptime(i['time'],'%I:%M%p')))
+	news_articles['news'].reverse()
 
-	json_string =  dumps(news_articles, indent=4)
-	out_json = open(file_to_write, 'w')
-	out_json.write(json_string)
+	if output_type == 'json':
+		json_string =  dumps(news_articles, indent=4)
+		out_json = open(file_to_write, 'w')
+		out_json.write(json_string)
 
-	out_json.close()
+		out_json.close()
+	else:	#csv
+		out_csv = open(file_to_write, 'w')
+		header_list = list(news_articles['news'][0].keys())
+		writer = csv.DictWriter(out_csv, fieldnames=header_list)
+		writer.writerow({'url': 'Last Updated', 'title': news_articles['updateTime'][0]['time'], 'source': '', 'date': '', 'time': ''})
+
+		writer.writeheader()
+
+		for diction in news_articles['news']:
+			writer.writerow(diction)
+
 	rss_file.close()
 
 if __name__ == '__main__':
@@ -65,6 +80,6 @@ if __name__ == '__main__':
 
 	# rss feeds link file
 	FNAME = 'rss_feeds.md'
-	FTOWRITE = 'scraped_news.json'
-	json_string = newscrawler(FNAME, HOUR_OFFSET, FILTER_BY, FTOWRITE)
+	FTOWRITE = 'scraped_news.csv'
+	json_string = newscrawler(FNAME, HOUR_OFFSET, FILTER_BY, FTOWRITE, output_type='csv')
 
